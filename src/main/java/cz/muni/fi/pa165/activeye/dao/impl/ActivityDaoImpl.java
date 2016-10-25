4b5fc3d28f4c383d96e5d168b78251432766bdb1
Matej Vasko <matej.vasko@outlook.com>
2016-10-25 20:55:23 +0200

    [MOD] ActivityDaoImpl that findAll method will return unmodifiable List

--- src/main/java/cz/muni/fi/pa165/activeye/dao/impl/ActivityDaoImpl.java
+++ src/main/java/cz/muni/fi/pa165/activeye/dao/impl/ActivityDaoImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -69,7 +69,7 @@ public class ActivityDaoImpl implements ActivityDao {
 
     @Override
     public List<Activity> findAll() {
-        return em.createQuery("SELECT a FROM Activity a").getResultList();
+        return Collections.unmodifiableList(em.createQuery("SELECT a FROM Activity a").getResultList());
     }
 
 }
