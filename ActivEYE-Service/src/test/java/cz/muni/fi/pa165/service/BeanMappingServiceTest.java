26464997e68ca964a919e08728a67c702fe36fbf
Matej Vasko <matej.vasko@outlook.com>
2016-11-21 22:30:48 +0100

    [ADD] empty facade test classes

---
+++ ActivEYE-Service/src/test/java/cz/muni/fi/pa165/service/BeanMappingServiceTest.java
@@ -0,0 +1,5 @@
+package cz.muni.fi.pa165.service;
+
+public class BeanMappingServiceTest {
+  // There must be extensive unit tests for the service layer (particularly for your 2 business functions) and all the tests of service layer must use Mock DAO objects;
+}
