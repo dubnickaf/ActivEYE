cd5ed0539806853b02e269cbd4923fdc2c00b4a9
Branislav Bajužík <xbajuzik@fi.muni.cz>
2016-11-25 23:50:20 +0100

    GroupService and GroupFacade javadocs

--- ActivEYE-API/src/main/java/cz/muni/fi/pa165/activeye/facades/GroupFacade.java
+++ ActivEYE-API/src/main/java/cz/muni/fi/pa165/activeye/facades/GroupFacade.java
@@ -10,27 +10,62 @@ import java.util.Collection;
 public interface GroupFacade {
 
     /**
-     * Create Group from GroupDTO
+     * Create Group
      * @param group group to be created
      * @throws IllegalArgumentException when group is null
      */
     void create(GroupDTO group);
 
     /**
-     *
-     * @param group
+     * Updates Group
+     * @param group group to be updated
+     * @throws IllegalArgumentException if group is null
      */
     void update(GroupDTO group);
 
+    /**
+     * Deletes Group
+     * @param group group to be deleted
+     * @throws IllegalArgumentException if group is null or wasn't persisted yet
+     */
     void delete(GroupDTO group);
 
+    /**
+     * Finds Group by id
+     * @param id id of the group to be found
+     * @return found group
+     * @throws IllegalArgumentException if id is null
+     * @throws cz.muni.fi.pa165.activeye.exceptions.NoSuchEntityFoundException if Group wasn't find
+     */
     GroupDTO findById(Long id);
 
+    /**
+     * Returns all groups
+     * @return all groups
+     * @throws cz.muni.fi.pa165.activeye.exceptions.NoSuchEntityFoundException if there are no groups
+     */
     Collection<GroupDTO> findAll();
 
+    /**
+     * Returns true if user is in Group, false otherwise
+     * @param user User to be found
+     * @param group Group to search in
+     * @return true if user is in Group, false otherwise
+     */
     boolean isUserInGroup(UserDTO user, GroupDTO group);
 
+    /**
+     * gets all users of group
+     * @param group Group to get Users of
+     * @return all users of the group
+     * @throws cz.muni.fi.pa165.activeye.exceptions.NoSuchEntityFoundException if there are no users in group
+     */
     Collection<UserDTO> getAllUsers(GroupDTO group);
 
+    /**
+     * Adds User to Group
+     * @param user User to be added
+     * @param group Group to be added to
+     */
     void addUser(UserDTO user, GroupDTO group);
 }
--- ActivEYE-Service/src/main/java/cz/muni/fi/pa165/activeye/service/GroupService.java
+++ ActivEYE-Service/src/main/java/cz/muni/fi/pa165/activeye/service/GroupService.java
@@ -9,19 +9,72 @@ import java.util.Collection;
  */
 public interface GroupService {
 
+    /**
+     * Creates Group
+     * @param group Group to be created
+     * @throws IllegalArgumentException if group is null
+     * @throws cz.muni.fi.pa165.activeye.exceptions.ActiveyeDataAccessException if there is error in DAO layer
+     */
     void create(Group group);
 
+    /**
+     * Updates Group
+     * @param group Group to be updated
+     * @throws IllegalArgumentException if group is null
+     * @throws cz.muni.fi.pa165.activeye.exceptions.ActiveyeDataAccessException if there is error in DAO layer
+     */
     void update(Group group);
 
+    /**
+     * Updates Group
+     * @param group Group to be updated
+     * @throws IllegalArgumentException if group is null
+     * @throws cz.muni.fi.pa165.activeye.exceptions.ActiveyeDataAccessException if there is error in DAO layer
+     */
     void delete(Group group);
 
+    /**
+     *
+     * @param id id of the Group to be found
+     * @return Found Group
+     * @throws IllegalArgumentException if id is null
+     * @throws cz.muni.fi.pa165.activeye.exceptions.ActiveyeDataAccessException if there is error in DAO layer
+     * @throws cz.muni.fi.pa165.activeye.exceptions.NoSuchEntityFoundException if The group isnt found
+     */
     Group findById(Long id);
 
+    /**
+     * Gets all groups
+     * @return All groups
+     * @throws cz.muni.fi.pa165.activeye.exceptions.ActiveyeDataAccessException if there is error in DAO layer
+     */
     Collection<Group> findAll();
 
+    /**
+     * returns true if User is in group, false otherwise
+     * @param user User to be found
+     * @param group Group to search in
+     * @return returns true if User is in group, false otherwise
+     * @throws IllegalArgumentException if group or user is null
+     * @throws cz.muni.fi.pa165.activeye.exceptions.ActiveyeDataAccessException if there is error in DAO layer
+     */
     boolean isUserInGroup(User user, Group group);
 
+    /**
+     * gets all users of a group
+     * @param group group to search in
+     * @return all users of a group
+     * @throws IllegalArgumentException if group is null
+     * @throws cz.muni.fi.pa165.activeye.exceptions.ActiveyeDataAccessException if there is error in DAO layer
+     */
     Collection<User> getAllUsers(Group group);
 
+    /**
+     * Adds User to Group
+     * @param user User to be added
+     * @param group Group to be added to
+     * @throws IllegalArgumentException if group or user is null
+     * @throws cz.muni.fi.pa165.activeye.exceptions.ActiveyeDataAccessException if there is error in DAO layer
+     */
     void addUser(User user, Group group);
 }
