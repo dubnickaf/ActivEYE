fd79b06ae1d9b47954a8cbd94e2c5741c0fe5d22
Filip Dubnička <dubnickaf@gmail.com>
2016-12-16 17:30:16 +0100

    records

--- ActivEYE-Webapp/src/main/webapp/js/controller/RecordsController.js
+++ ActivEYE-Webapp/src/main/webapp/js/controller/RecordsController.js
@@ -1,7 +1,18 @@
-
 angular.module('mainApp').controller('RecordsController',RecordsController);
 
 
-function RecordsController(){
+function RecordsController($rootScope, Session, UserService, $scope){
+    $scope.userWithRecords;
+    $scope.loadData = loadData();
+    function loadData() {
+        console.log("usertouse",Session.getUser());
+        UserService.findWithRecordsByEmail(Session.getUser().emailAddress).then(function(data){
+
+            $scope.userWithRecords = data.data;
+            console.log($scope.userWithRecords);
+        });
+        console.log($scope.userWithRecords);
+    }
+    loadData();
 
 }
