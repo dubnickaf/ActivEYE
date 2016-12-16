7f42c3e8a010820c70115cfdaf533178c0bee440
Branislav Bajužík <xbajuzik@fi.muni.cz>
2016-12-16 16:25:34 +0100

    GroupsControler

--- ActivEYE-Webapp/src/main/webapp/js/controller/GroupsController.js
+++ ActivEYE-Webapp/src/main/webapp/js/controller/GroupsController.js
@@ -1,7 +1,16 @@
 
 angular.module('mainApp').controller('GroupsController',GroupsController);
 
+GroupsController.$inject = ['$rootScope','GroupService','$scope','mediator','Session','Router'];
 
-function GroupsController(){
-
+function GroupsController($rootScope, GroupService, $scope, mediator, Session, Router) {
+    console.log("aaaaa");
+    $scope.loadData2 = loadData2();
+    function loadData2() {
+        console.log(Session);
+        GroupService.findAll().then(function(data) {
+            console.log(data);
+        });
+    }
+    loadData2();
 }
