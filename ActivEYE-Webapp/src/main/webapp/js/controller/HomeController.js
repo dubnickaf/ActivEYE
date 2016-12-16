aa5a7c4c1f445c699436fe26ea91961f3119a5c2
Filip Dubnička <dubnickaf@gmail.com>
2016-12-16 23:36:52 +0100

    Unable to load home pages logged out

--- ActivEYE-Webapp/src/main/webapp/js/controller/HomeController.js
+++ ActivEYE-Webapp/src/main/webapp/js/controller/HomeController.js
@@ -1,7 +1,16 @@
 angular.module('mainApp').controller('HomeController',HomeController);
 
 function HomeController($rootScope,UserService,$scope,mediator,Session,Router){
-    $scope.userEmailAddress = Session.getUser().emailAddress;
-    $scope.userName = Session.getUser().name;
-
+    $scope.redirectIfLoggedOut = function() {
+        if(Session.isAnyoneLoggedIn() === false) {
+            Router.redirect('/login');
+        }else{
+            $scope.userEmailAddress = Session.getUser().emailAddress;
+            $scope.userName = Session.getUser().name;
+        }
+    };
+    $scope.redirectIfLoggedOut();
+    $scope.logOutConsequences = function() {
+        Session.saveUser(undefined);
+    }
 }
--- ActivEYE-Webapp/src/main/webapp/js/controller/RecordsController.js
+++ ActivEYE-Webapp/src/main/webapp/js/controller/RecordsController.js
@@ -5,13 +5,13 @@ RecordsController.$inject = ['RecordService','Session','UserService','$scope'];
 function RecordsController(RecordService,Session, UserService, $scope){
 
     $scope.userWithRecords = undefined;
-    $scope.deleteRecord = deleteRecord();
-    function deleteRecord(record){
+    $scope.deleteRecord = function(record){
+            console.log("Deleting record ", record);
             RecordService.delete(record).then(function(){
-                console.log($scope.userWithRecords);
+                console.log("Deleting successful");
                 Router.redirect('home/dashboard/records');
             });
-    }
+    };
     $scope.loadData = loadData();
     function loadData() {
         console.log("usertouse",Session.getUser());
