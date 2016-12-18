98c2d5491406ce9592860e1e2f1b466e0584ab3d
Branislav Bajužík <xbajuzik@fi.muni.cz>
2016-12-18 21:03:56 +0100

    groups

--- ActivEYE-Webapp/src/main/webapp/js/patternfly/CreateGroupSelectController.js
+++ ActivEYE-Webapp/src/main/webapp/js/patternfly/CreateGroupSelectController.js
@@ -8,9 +8,12 @@ CreateGroupSelectController.$inject = ['$scope','UserService'];
 
 function CreateGroupSelectController($scope,UserService){
     $scope.allUsers = undefined;
+    $scope.multipleSelect = [];
     $scope.init = function() {
         UserService.getAll().then(function(res){
             $scope.allUsers = res.data;
+            console.log("$scope.multipleSelect:");
+            console.log($scope.multipleSelect);
         })
     };
     $scope.init();
